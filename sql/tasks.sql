bdd7be884ca95ba9cfa1ebc37d1e87a4f1a4908c
David van Oudheusden <doudheusden@novacollege.nl>
2024-08-29 14:37:09 +0200

    added db

---
+++ sql/tasks.sql
@@ -0,0 +1,20 @@
+
+--
+-- Tabelstructuur voor tabel `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `category` varchar(200) DEFAULT NULL,
+  `status` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `title`, `category`, `status`) VALUES
+(1, 'Boodschappen doen deze week', NULL, 'in behandeling'),
+(2, 'Autoverzekering vernieuwen', NULL, 'in behandeling'),
+(3, 'Online code cursus kopen', 'studeren', 'in behandeling');
